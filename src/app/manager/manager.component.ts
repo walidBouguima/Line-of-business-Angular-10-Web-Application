b300fc2d55d9a1fc07fa353b97dc2bbe7623c089
walidbouguima <walid.bouguima@gmail.com>
2020-10-30 17:08:14 +0100

    Create two new components to populate the newly added navbar in the manager module

--- src/app/manager/manager.component.ts
+++ src/app/manager/manager.component.ts
@@ -3,11 +3,37 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-manager',
   template: `
-    <p>
-      manager works!
-    </p>
+  <mat-toolbar color="accent">
+    <a mat-button
+    routerLink="/manager/home"
+    routerLinkActivate="active-link">
+    Manager's Dashboard
+    </a>
+    <a mat-button
+    routerLink="/manager/users"
+    routerLinkActivate="active-link">
+    User Management
+    </a>
+    <a mat-button
+    routerLink="/manager/receipts"
+    routerLinkActivate="active-link">
+    Receipt Lookup
+    </a>
+
+  </mat-toolbar>
   `,
   styles: [
+    `
+    div[fxLayout]{
+      margin-top: 32px;
+    }
+    `,
+    `
+    .active-link {
+      font-weight: bold;
+      border-bottom: 2px solid #005005;
+    }
+    `
   ]
 })
 export class ManagerComponent implements OnInit {
--- src/app/manager/manager.module.ts
+++ src/app/manager/manager.module.ts
@@ -1,12 +1,24 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
+import { MaterialModule } from '../material.module'
 
 import { ManagerHomeComponent } from './manager-home/manager-home.component'
 import { ManagerRoutingModule } from './manager-routing.module';
-import { ManagerComponent } from './manager.component'
+import { ManagerComponent } from './manager.component';
+import { UserManagementComponent } from './user-management/user-management.component';
+import { ReceiptLookupComponent } from './receipt-lookup/receipt-lookup.component'
 
 @NgModule({
-  declarations: [ManagerHomeComponent, ManagerComponent],
-  imports: [CommonModule, ManagerRoutingModule],
+  declarations: [
+    ManagerHomeComponent,
+    ManagerComponent,
+    UserManagementComponent,
+    ReceiptLookupComponent
+  ],
+  imports: [
+    CommonModule,
+    ManagerRoutingModule,
+    MatrialModule
+  ],
 })
 export class ManagerModule {}
