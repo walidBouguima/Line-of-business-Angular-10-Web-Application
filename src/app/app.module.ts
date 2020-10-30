a7beb883657d85a85347af5980a5556f0a6fe6d7
walidbouguima <walid.bouguima@gmail.com>
2020-10-30 11:23:32 +0100

    Generate a home c

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
-import { MaterialModule } from './material.module';
-import { ManagerModule } from './manager/manager.module';
-import { InventoryModule } from './inventory/inventory.module';
-import { PosModule } from './pos/pos.module';
-import { UserModule } from './user/user.module'
+import { InventoryModule } from './inventory/inventory.module'
+import { ManagerModule } from './manager/manager.module'
+import { MaterialModule } from './material.module'
+import { PosModule } from './pos/pos.module'
+import { UserModule } from './user/user.module';
+import { HomeComponent } from './home/home.component'
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
