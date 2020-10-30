88477d303ef454d8779d6f45a2dd1f98988f78d0
walidbouguima <walid.bouguima@gmail.com>
2020-10-30 16:55:50 +0100

    Generate a manager component to replicate the home app relationship between the manager and manager home so that we have a consistent UX with fixed Nav Bar

--- src/app/manager/manager.module.ts
+++ src/app/manager/manager.module.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 
 import { ManagerHomeComponent } from './manager-home/manager-home.component'
-import { ManagerRoutingModule } from './manager-routing.module'
+import { ManagerRoutingModule } from './manager-routing.module';
+import { ManagerComponent } from './manager.component'
 
 @NgModule({
-  declarations: [ManagerHomeComponent],
+  declarations: [ManagerHomeComponent, ManagerComponent],
   imports: [CommonModule, ManagerRoutingModule],
 })
 export class ManagerModule {}
