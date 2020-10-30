f3bb94e141a0573c2f8c19e49da1b2254c909a09
walidbouguima <walid.bouguima@gmail.com>
2020-10-30 11:14:50 +0100

    Generate the basic modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
-import { MaterialModule } from './material.module'
+import { MaterialModule } from './material.module';
+import { ManagerModule } from './manager/manager.module';
+import { InventoryModule } from './inventory/inventory.module';
+import { PosModule } from './pos/pos.module';
+import { UserModule } from './user/user.module'
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,6 +19,10 @@ import { MaterialModule } from './material.module'
     BrowserAnimationsModule,
     MaterialModule,
     FlexLayoutModule,
+    ManagerModule,
+    InventoryModule,
+    PosModule,
+    UserModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
