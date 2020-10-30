3a8b7842d72a6071738dd0d9e4183baa57dc8bf6
walidbouguima <walid.bouguima@gmail.com>
2020-10-30 12:46:54 +0100

    Add material icons to toolbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: '/home', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
   { path: '**', component: PageNotFoundComponent },
 ]
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core'
+import { MatIconRegistry } from '@angular/material/icon'
+import { DomSanitizer } from '@angular/platform-browser'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,10 @@ import { Component } from '@angular/core'
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'lemon-mart'
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIcon(
+      'lime',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/img/lime.svg')
+    )
+  }
 }
