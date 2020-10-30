b0a5a83c8e542a45ba68df8f62b268a7ca2a00aa
walidbouguima <walid.bouguima@gmail.com>
2020-10-30 18:58:00 +0100

    Add a navbar in the manager feature module with set of components lazely loaded

--- src/app/manager/manager.component.ts
+++ src/app/manager/manager.component.ts
@@ -1,46 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 
 @Component({
   selector: 'app-manager',
   template: `
-  <mat-toolbar color="accent">
-    <a mat-button
-    routerLink="/manager/home"
-    routerLinkActivate="active-link">
-    Manager's Dashboard
-    </a>
-    <a mat-button
-    routerLink="/manager/users"
-    routerLinkActivate="active-link">
-    User Management
-    </a>
-    <a mat-button
-    routerLink="/manager/receipts"
-    routerLinkActivate="active-link">
-    Receipt Lookup
-    </a>
-
-  </mat-toolbar>
+    <mat-toolbar color="accent">
+      <a mat-button routerLink="/manager/home" routerLinkActive="active-link">
+        Manager's Dashboard
+      </a>
+      <a mat-button routerLink="/manager/users" routerLinkActive="active-link">
+        User Management
+      </a>
+      <a mat-button routerLink="/manager/receipts" routerLinkActive="active-link">
+        Receipt Lookup
+      </a>
+    </mat-toolbar>
+    <router-outlet></router-outlet>
   `,
   styles: [
     `
-    div[fxLayout]{
-      margin-top: 32px;
-    }
+      div[fxLayout] {
+        margin-top: 32px;
+      }
     `,
     `
-    .active-link {
-      font-weight: bold;
-      border-bottom: 2px solid #005005;
-    }
-    `
-  ]
+      .active-link {
+        font-weight: bold;
+        border-bottom: 2px solid #005005;
+      }
+    `,
+  ],
 })
 export class ManagerComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/manager/manager.module.ts
+++ src/app/manager/manager.module.ts
@@ -1,24 +1,20 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
-import { MaterialModule } from '../material.module'
 
+import { MaterialModule } from '../material.module'
 import { ManagerHomeComponent } from './manager-home/manager-home.component'
-import { ManagerRoutingModule } from './manager-routing.module';
-import { ManagerComponent } from './manager.component';
-import { UserManagementComponent } from './user-management/user-management.component';
+import { ManagerRoutingModule } from './manager-routing.module'
+import { ManagerComponent } from './manager.component'
 import { ReceiptLookupComponent } from './receipt-lookup/receipt-lookup.component'
+import { UserManagementComponent } from './user-management/user-management.component'
 
 @NgModule({
   declarations: [
     ManagerHomeComponent,
     ManagerComponent,
     UserManagementComponent,
-    ReceiptLookupComponent
+    ReceiptLookupComponent,
   ],
-  imports: [
-    CommonModule,
-    ManagerRoutingModule,
-    MatrialModule
-  ],
+  imports: [CommonModule, ManagerRoutingModule, MaterialModule],
 })
 export class ManagerModule {}
