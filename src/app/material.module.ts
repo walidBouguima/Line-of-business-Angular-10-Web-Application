e82fff2ff5425449cc16ee1a79e7628bab492743
walidbouguima <walid.bouguima@gmail.com>
2020-11-01 14:56:14 +0100

    Add FlexLayoutModule to the material module

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core'
+import { FlexLayoutModule } from '@angular/flex-layout'
 import { MatButtonModule } from '@angular/material/button'
 import { MatIconModule } from '@angular/material/icon'
 import { MatToolbarModule } from '@angular/material/toolbar'
 import { MatTooltipModule } from '@angular/material/tooltip'
-
-const modules = [MatButtonModule, MatToolbarModule, MatTooltipModule, MatIconModule]
+const modules = [
+  MatButtonModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatIconModule,
+  FlexLayoutModule,
+]
 @NgModule({
   declarations: [],
   imports: [modules],
