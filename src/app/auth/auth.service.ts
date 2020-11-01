8e9217acf9f5379727f8dc803f935c5304ee6ccb
walidbouguima <walid.bouguima@gmail.com>
2020-11-01 16:47:38 +0100

    Define the defaultAuthStatus and instantiate it

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,22 @@
 import { Injectable } from '@angular/core'
 
+import { Role } from './auth.enum'
+
+export interface IAuthStatus {
+  isAuthenticated: boolean
+  userRole: Role
+  userId: string
+}
+export interface IServerAuthResponse {
+  accessToken: string
+}
+
+export const defaultAuthStatus: IAuthStatus = {
+  isAuthenticated: false,
+  userRole: Role.None,
+  userId: '',
+}
+
 @Injectable()
 export class AuthService {
   constructor() {}
