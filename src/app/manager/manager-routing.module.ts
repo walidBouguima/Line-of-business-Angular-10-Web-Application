a2d86f6e53bc9800f0914c606acac85bc75580df
walidbouguima <walid.bouguima@gmail.com>
2020-11-06 19:37:13 +0100

    Add auth guards with pertinent routes

--- src/app/manager/manager-routing.module.ts
+++ src/app/manager/manager-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 
+import { AuthGuardService } from '../auth/auth-guard.service'
+import { Role } from '../auth/auth.enum'
 import { ManagerHomeComponent } from './manager-home/manager-home.component'
 import { ManagerComponent } from './manager.component'
 import { ReceiptLookupComponent } from './receipt-lookup/receipt-lookup.component'
@@ -12,9 +14,19 @@ const routes: Routes = [
     component: ManagerComponent,
     children: [
       { path: '', redirectTo: '/manager/home', pathMatch: 'full' },
-      { path: 'home', component: ManagerHomeComponent },
+      {
+        path: 'home',
+        component: ManagerHomeComponent,
+        canActivate: [AuthGuardService],
+        data: { expectedRole: Role.Manager },
+      },
       { path: 'users', component: UserManagementComponent },
-      { path: 'receipts', component: ReceiptLookupComponent },
+      {
+        path: 'receipts',
+        component: ReceiptLookupComponent,
+        canActivate: [AuthGuardService],
+        data: { expectedRole: Role.Manager },
+      },
     ],
   },
 ]
--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 
+import { AuthGuardService } from '../auth/auth-guard.service'
 import { LogoutComponent } from './logout/logout.component'
 import { ProfileComponent } from './profile/profile.component'
 
 const routes: Routes = [
-  { path: 'profile', component: ProfileComponent },
+  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuardService] },
   { path: 'logout', component: LogoutComponent },
 ]
 
